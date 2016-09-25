76747939d6a89c33b99ccba08b9c42e05bd4cf94
zgs225 <zgs225@gmail.com>
2016-09-26 07:18:34 +0800

    添加执行命令的流程

--- lucky/commander.py
+++ lucky/commander.py
@@ -1,4 +1,34 @@
 # -*- coding: utf-8 -*-
 
 class Commander(object):
-    pass
+    """
+    用于保存命令的字典，对应：
+    command: function
+    """
+    commands = {}
+
+    def __init__(self, *args):
+        """
+        Keyword Arguments:
+        *args -- array of arguments like sys.argv
+        """
+        self.args = args[1:]
+        self.action = args[1]
+
+    def register(self, cmd):
+        """
+        注册命令的装饰器
+        """
+        def decorator(f):
+            self.commands[cmd] = f
+            return f
+        return decorator
+
+    def execute(self):
+        func = self.commands[self.action]
+        func(*self.args)
+
+def install_commands(cmder):
+    @cmder.register('new_post')
+    def new_post(*args):
+        print(args)
